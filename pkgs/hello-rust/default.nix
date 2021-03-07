bc31ceba5f550a8419c1defd1142113b5e22c39e
Michael Brantley <limeytexan@users.noreply.github.com>
2021-03-07 13:18:44 +0000

    hello-rust: updated cargoSha256
    On account of https://github.com/flox/nixexprs/pull/28.

--- pkgs/hello-rust/default.nix
+++ pkgs/hello-rust/default.nix
@@ -1,5 +1,5 @@
 { flox }:
 flox.buildRustPackage {
   project = "hello-rust";
-  cargoSha256 = "04xplfik6vhi481n06awbs9mrycg0cxwn299a0z2ysc4v4n68qcw";
+  cargoSha256 = "00vi4ybmf9l5ambwqcmkn9m9dhz3vh9f4hwq938fkb4xgxnil8w1";
 }
