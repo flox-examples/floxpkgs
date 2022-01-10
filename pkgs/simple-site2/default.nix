3b9ed36119a111e964289e31b7c47250a8628f77
Michael Brantley <limeytexan@users.noreply.github.com>
2022-01-10 23:38:03 +0000

    simple-site2: adding a second package built with evil yarn

---
+++ pkgs/simple-site2/default.nix
@@ -0,0 +1,17 @@
+{ mkYarnPackage, ran, runtimeShell }:
+
+mkYarnPackage {
+  project = "simple-site";
+  postBuild = ''
+    pushd deps/simple-site
+    yarn run build
+    popd
+  '';
+  postInstall = ''
+    cat > $out/bin/launch-simple-site <<EOF
+    #!${runtimeShell} -x
+    exec ${ran}/bin/ran -r $out/libexec/simple-site/deps/simple-site/public "\$@"
+    EOF
+    chmod +x $out/bin/launch-simple-site
+  '';
+}
