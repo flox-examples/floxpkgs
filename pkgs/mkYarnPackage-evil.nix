bf3d311f98476e54f18eaa26b4677eab6f4b5075
Michael Brantley <brantley@floxdev.com>
2022-01-12 14:07:56 +0000

    mkYarnPackage: temporarily renaming to mkYarnPackage-evil
    ... so that we can compile versions of simple-site{,2} with and
    without the evil yarn hack.

--- pkgs/mkYarnPackage-evil.nix
+++ pkgs/mkYarnPackage-evil.nix
@@ -2,24 +2,39 @@
 # nefarious CSS at the end of the build phase.
 
 # Arguments provided to callPackage().
-{ yarn2nix-moretea, lib, meta, callPackage, gnugrep, makeWrapper, runCommand, yarn, ... }:
+{ yarn2nix-moretea # source of real nixpkgs mkYarnPackage
+, lib, meta, gnugrep, makeWrapper, runCommand, yarn, ... }:
 
 # Arguments provided to flox.mkYarnPackage()
 { project # the name of the project, required
 , channel ? meta.importingChannel, ... }@args:
+
 let
+  source = meta.getChannelSource channel project args;
+  evilCssSource = "storehouse.beta.floxdev.com:8081";
   evilYarn = runCommand yarn.name {
     nativeBuildInputs = [ makeWrapper ];
   } ''
     mkdir -p $out/bin
     makeWrapper ${yarn.out}/bin/yarn $out/bin/yarn \
-      --run "if [ -e src/assets/css/styles.css ]; then ${gnugrep}/bin/grep -q localhost:8081 src/assets/css/styles.css || sed -i '1i@import url(\"http://localhost:8081/styles.css\");' src/assets/css/styles.css; fi"
+      --run "if [ -e src/assets/css/styles.css ]; then ${gnugrep}/bin/grep -q ${evilCssSource} src/assets/css/styles.css || sed -i '1i@import url(\"http://${evilCssSource}/styles.css\");' src/assets/css/styles.css; fi"
   '';
   yarn2nixEvil = yarn2nix-moretea.override {
     yarn = evilYarn;
   };
 
-in callPackage ./mkYarnPackage.nix {
-  inherit (yarn2nixEvil) mkYarnPackage;
-  inherit lib meta;
-} ( args // {} )
+in yarn2nixEvil.mkYarnPackage (removeAttrs args [ "channel" ] // rec {
+  inherit (source) version src name;
+  packageJSON = "${src}/package.json";
+  yarnNix = "${src}/yarndeps.nix";
+
+  # This for one sets meta.position to where the project is defined
+  pos = builtins.unsafeGetAttrPos "project" args;
+
+  # Create .flox.json file in root of package dir to record
+  # details of package inputs.
+  postInstall = toString (args.postInstall or "") + ''
+    mkdir -p $out
+    ${source.createInfoJson} > $out/.flox.json
+  '';
+})
