e4cb967f717bb2dacedfa475b3a3fcff08eece75
Michael Brantley <brantley@floxdev.com>
2022-12-07 21:12:22 +0000

    flox.nix: enable hydra jobs
    Temporary: to be refactored into a simple boolean attribute.

--- flox.nix
+++ flox.nix
@@ -4,6 +4,18 @@
   lib,
   ...
 }:
+
+  # re-call yourself with overrides, will not work if using in-memory lockfile
+  let hydraOverride = path: follows: (inputs.flox-floxpkgs.capacitor.lib.capacitor.callFlake
+    (builtins.readFile (self + "/flake.lock"))
+    self "" "" "root" { }
+    [ {
+        path = path;
+        follows = follows;
+      } ]
+    ).hydraJobs;
+  in
+
 # Define package set structure
 {
 
@@ -17,4 +29,8 @@
       )
       (inputs.flox-floxpkgs.capacitor.plugins.allLocalResources {})
     ];
+
+  passthru."hydraJobsStaging" = hydraOverride ["nixpkgs" "nixpkgs"] ["nixpkgs" "nixpkgs-staging"];
+  passthru."hydraJobsUnstable" = hydraOverride ["nixpkgs" "nixpkgs"] ["nixpkgs" "nixpkgs-unstable"];
+  passthru."hydraJobsStable" = hydraOverride ["nixpkgs" "nixpkgs"] ["nixpkgs" "nixpkgs-stable"];
 }
