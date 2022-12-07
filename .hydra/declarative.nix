ea28a0d2cf16345a06a4727d9ba7eec28c113d06
Michael Brantley <brantley@floxdev.com>
2022-12-07 12:50:21 +0000

    .hydra: added declarative jobset specfiles

---
+++ .hydra/declarative.nix
@@ -0,0 +1,39 @@
+{
+  pkgs ? (builtins.getFlake "nixpkgs").legacyPackages.x86_64-linux,
+  ...
+}: let
+  mkFlakeJobset = branch: stability: {
+    description = "Packages built with nixpkgs ${pkgs.lib.toLower stability}";
+    checkinterval = "600";
+    enabled = "1";
+    schedulingshares = 100;
+    enableemail = false;
+    emailoverride = "";
+    keepnr = 3;
+    hidden = false;
+    type = 1;
+    flake = "https://github.com/flox/floxpkgs?ref=${branch}";
+    flakeattr = "hydraJobs${stability}";
+  };
+
+  desc = {
+    "stable" = mkFlakeJobset "master" "Stable";
+    "staging" = mkFlakeJobset "master" "Staging";
+    "unstable" = mkFlakeJobset "master" "Unstable";
+  };
+
+  log = {
+    jobsets = desc;
+  };
+in {
+  jobsets = pkgs.runCommand "spec-jobsets.json" {} ''
+    cat >$out <<EOF
+    ${builtins.toJSON desc}
+    EOF
+    # This is to get nice .jobsets build logs on Hydra
+    cat >tmp <<EOF
+    ${builtins.toJSON log}
+    EOF
+    ${pkgs.jq}/bin/jq . tmp
+  '';
+}
