9098cd25d04b1a24d2d32e80829ed9a0ceabf9e5
Alex Ameen <alex.ameen.tx@gmail.com>
2023-07-18 16:09:09 -0500

    fix: handle rev in build-repo

--- pub.sh
+++ pub.sh
@@ -5,15 +5,34 @@ set -o pipefail;
 
 PKG="${1?You must provide a package name}";
 
+# Utilities
 : "${NIX:=nix}";
 : "${FLOX:=flox}";
+: "${GIT:=git}";
 : "${REALPATH:=$NIX shell nixpkgs#coreutils -c realpath}";
+
+# Default settings
 : "${OWNER:=flox-examples}";
 : "${REPO:=floxpkgs}";
 : "${STABILITY:=stable}";
 
 SPATH="$( $REALPATH "${BASH_SOURCE[0]}"; )";
 SDIR="${SPATH%/*}";
+
+: "${BRANCH:=$( $GIT branch -C "$SDIR" --show-current; )}";
+
+# Check for dirty tree
+if ! $GIT diff -C "$SDIR" "origin/$BRANCH" --quiet 2>/dev/null; then
+  echo "pub.sh: Tree is dirty or unpushed. Commit/push, and try again." >&2;
+  exit 1;
+fi
+
+: "${REV:=$( $GIT rev-parse HEAD; )}";
+
+# Check for unpushed
+if ! $GIT diff -C "$SDIR" --quiet 2>/dev/null; then
+  echo "pub.sh: Tree is dirty, commit, push, and try again." >&2;
+fi
 
 if ! [[ -d "$SDIR/pkgs/$PKG" ]]; then
   echo "pub.sh: No such package: '$PKG'" >&2;
@@ -22,11 +41,11 @@ fi
 
 : "${SYSTEM:=$( $NIX eval --raw --impure --expr builtins.currentSystem; )}";
 
-$FLOX                                                 \
-    --stability "$STABILITY"                          \
-    publish                                           \
-    --attr "packages.$SYSTEM.$PKG"                    \
-    --publish-system "$SYSTEM"                        \
-    --build-repo "git@github.com:$OWNER/$REPO.git"    \
-    --channel-repo "git@github.com:$OWNER/$REPO.git"  \
+$FLOX                                                        \
+    --stability "$STABILITY"                                 \
+    publish                                                  \
+    --attr "packages.$SYSTEM.$PKG"                           \
+    --publish-system "$SYSTEM"                               \
+    --build-repo "git@github.com:$OWNER/$REPO.git?rev=$REV"  \
+    --channel-repo "git@github.com:$OWNER/$REPO.git"         \
   ;
